b89de771463a0e1ae31c9f5d2a0fbfb19b9412b9
nofal mami <naoufel.mami@gmail.com>
2019-11-06 10:54:53 +0100

    for meeting app

--- meeting/config/docs.py
+++ meeting/config/docs.py
@@ -2,10 +2,10 @@
 Configuration for docs
 """
 
-# source_link = "https://github.com/[org_name]/meeting"
-# docs_base_url = "https://[org_name].github.io/meeting"
-# headline = "App that does everything"
-# sub_heading = "Yes, you got that right the first time, everything"
+source_link = "https://github.com/Nofal-Mami/meeting"
+docs_base_url = "https://Nofal-Mami.github.io/meeting"
+headline = "Meeting management"
+sub_heading = "Meeting management"
 
 def get_context(context):
 	context.brand_html = "Meeting"
